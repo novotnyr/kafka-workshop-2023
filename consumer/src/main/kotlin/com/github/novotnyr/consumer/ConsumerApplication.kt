84a34d97aabf6f1d34e83b4ee5b50e8a312d399f
Robert Novotny <novotnr0@gmail.com>
2023-11-11 18:37:01 +0100

    Consumer: Add 2 backoffs with initial 5 seconds delay on failed records

--- consumer/src/main/kotlin/com/github/novotnyr/consumer/ConsumerApplication.kt
+++ consumer/src/main/kotlin/com/github/novotnyr/consumer/ConsumerApplication.kt
@@ -2,9 +2,16 @@ package com.github.novotnyr.consumer
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.kafka.listener.DefaultErrorHandler
+import org.springframework.util.backoff.FixedBackOff
+
 
 @SpringBootApplication
-class ConsumerApplication
+class ConsumerApplication {
+    @Bean
+    fun kafkaErrorHandler() = DefaultErrorHandler(FixedBackOff(5000L, 2L))
+}
 
 fun main(args: Array<String>) {
     runApplication<ConsumerApplication>(*args)
